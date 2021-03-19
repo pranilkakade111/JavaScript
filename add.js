822d96fbaded3683016eb1a7978e44854843aea6
Pranil Kakade <pranilkakade111@gmail.com>
2021-03-20 00:02:28 +0530

    Test_Code

---
+++ add.js
@@ -0,0 +1,17 @@
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c);
+console.log(typeof a);
+
+let s1="10";
+let s2="20";
+let s3=s1+s2;
+console.log(s3);
+
+let d= new Date();
+let today="Today is :" + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+console.log(today);
+let time="Time is =" + d.getHours() + ":" + d.getMinutes();
+console.log(time);
+console.log(typeof time);
